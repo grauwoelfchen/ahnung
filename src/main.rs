58757ac81bae71f11d5d43f088036b8db631cb50
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-10-09 12:41:54 +0900

    Add a command using structopt

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate structopt;
+
+use structopt::StructOpt;
+
+/// Command line options.
+#[derive(Debug, StructOpt)]
+#[structopt(
+  name = "ahnung",
+  about = "\nAn utility command for AST Hash of Rust codes."
+)]
+struct Opts {
+  /// Filename
+  #[structopt(short = "f", long = "file", default_value = "")]
+  file: String,
+}
+
+fn main() {
+    let opts = Opts::from_args();
+    println!("file: {file}", file = opts.file)
+}
